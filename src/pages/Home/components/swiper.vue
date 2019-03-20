dd16f707369367583b7e31cdf2e016e5a7888bb6
yellow3 <1309832228@qq.com>
2019-03-20 17:29:23 +0800

    vue init finish

---
+++ src/pages/Home/components/swiper.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">swiper</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper'
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
---
+++ src/pages/Home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home-swiper></home-swiper>
+  </div>
+</template>
+
+<script>
+import HomeSwiper from './components/swiper';
+export default {
+  name: 'Home',
+  components: {
+    HomeSwiper
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
